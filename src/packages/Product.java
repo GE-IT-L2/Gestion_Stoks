c42011c46c2f005c900c6cb4c926d07279e0973f
Tiavina <xenderonepiece@gmail.com>
2026-02-23 08:23:40 +0300

    MAJ Product.java et Type_product.java

--- src/packages/Product.java
+++ src/packages/Product.java
@@ -5,9 +5,40 @@ public class Product {
     private double prixUnitaire;
     private String unite; //gros na detail
     private int stock;
+    private Type_product type_product;
 
-    
+    public Product (int id, int stock, String designation, double prixUnitaire, String unite, Type_product type_product){
+        this.id = id;
+        this.designation = designation;
+        this.prixUnitaire = prixUnitaire;
+        this.unite = unite;
+        this.stock = stock;
+        this.type_product = type_product;
+    }
 
 
+    //Pour lire les variables private il faut des accesseur (getteurs)
+    public int getStock(){
+        return stock;
+    }
+    public double getPrixUnitaire(){
+        return prixUnitaire;
+    }
+    public String getDesignation(){
+        return designation;
+    }
 
+
+    //Methode
+    public void ajouterStock(int quantite){
+        this.stock += quantite;
+    }
+
+    public void retirerStock(int quantite){
+        if (quantite <= stock){
+            this.stock -= quantite;
+        }else{
+            System.out.println("Stock insuffisant");
+        }
+    }
 }
--- src/packages/Type_product.java
+++ src/packages/Type_product.java
@@ -1,5 +1,15 @@
 package packages;
 
 public class Type_product {
-    
+    private int id;
+    private String nom;
+
+    public Type_product(int id, String nom){
+        this.id = id;
+        this.nom = nom;
+    }
+
+    public String getNom(){
+        return nom;
+    }
 }
